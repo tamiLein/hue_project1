cccf495f0a4a995944bee524eaa898f9f657b81a
Tamara Czakert <Tamara Czakert>
2017-04-22 12:20:54 +0200

    chat works other vue elements work too but are not rendered into the template

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,40 @@
 let gulp = require('gulp');
+let webpackStream = require('webpack-stream');
+let webpack = require('webpack');
 let cssmin = require('gulp-cssmin');
+let sass = require('gulp-sass');
 let uglify = require('gulp-uglify');
+let named = require('vinyl-named');
+let gutil = require('gulp-util');
+let plumber = require('gulp-plumber');
+let args = require('yargs').argv;
+let nunjucksRender = require('gulp-nunjucks-render');
 let autoprefixer = require('gulp-autoprefixer');
 let browserSync = require('browser-sync').create();
 
-gulp.task('css', function() {
-  gulp.src('src/css/**/*.css')
-    .pipe(autoprefixer())
-    .pipe(cssmin())
-    .pipe(gulp.dest('build/css'));
+const env = (args.deploy) ? 'prod' : 'dev';
+console.log('using env:', env);
+let webpackConfig = require('./webpack.config.' + env);
+
+if (env != 'prod') {
+    cssmin = gutil.noop;
+}
+
+gulp.task('css', function () {
+    gulp.src('src/css/**/*.css')
+        .pipe(plumber())
+        .pipe(cssmin())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('scss', function () {
+    gulp.src('src/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(cssmin())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('font', function() {
@@ -16,20 +42,28 @@ gulp.task('font', function() {
         .pipe(gulp.dest('build/font'));
 });
 
-gulp.task('js', function() {
-  gulp.src('src/js/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
+gulp.task('js', function () {
+    gulp.src('src/js/*.js')
+        .pipe(plumber())
+        .pipe(named())
+        .pipe(webpackStream(webpackConfig, webpack))
+        .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('templates', function() {
-  gulp.src('src/templates/**/*.html')
-    .pipe(gulp.dest('build'));
+
+gulp.task('templates', function () {
+    gulp.src('src/templates/*.html')
+        .pipe(plumber())
+        .pipe(nunjucksRender({
+            path: 'src/templates'
+        }))
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('img', function() {
-  gulp.src('src/img/**/*')
-    .pipe(gulp.dest('build/img'));
+
+gulp.task('img', function () {
+    gulp.src('src/img/**/*')
+        .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('json', function() {
@@ -37,20 +71,23 @@ gulp.task('json', function() {
         .pipe(gulp.dest('build/json'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/css/**/*.css', ['css']);
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('src/templates/**/*.html', ['templates']);
+gulp.task('watch', function () {
+    // legacy task. scss does the job now
+    //gulp.watch('src/css/**/*.css', ['css']);
+    gulp.watch('src/scss/**/*', ['scss']);
+    gulp.watch(['src/js/**/*'], ['js']);
+    gulp.watch('src/templates/**/*', ['templates']);
 });
 
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./build"
-    },
-    files: ['./build/**/*'],
-    reloadDebounce: 500
-  });
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        },
+        files: ['./build/**/*'],
+        reloadDebounce: 500,
+        open: false
+    });
 });
 
 gulp.task('default', ['css', 'font', 'js', 'templates', 'img', 'json']);
---
+++ src/js/firebase.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Tamara on 22.04.2017.
+ */
+const config = {
+    apiKey: "AIzaSyA3IHkmPGAJKNEoI-CbtbT8o3BrWZeT-1g",
+    authDomain: "suta-chat.firebaseapp.com",
+    databaseURL: "https://suta-chat.firebaseio.com",
+    projectId: "suta-chat",
+    storageBucket: "suta-chat.appspot.com",
+    messagingSenderId: "716768886883"
+};
+
+//alert('Please add your config into the src/js/firebase.config.js');
+
+export default config;
